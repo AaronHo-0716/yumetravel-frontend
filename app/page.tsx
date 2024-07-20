d2e39e8d9eebbb34be89ddc54c174b1c2f4779fa
Aaron Ho <hoyanxun@gmail.com>
2024-07-20 19:50:09 +0800

    feat(api): Added essential functions

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import { Input } from '@/components/ui/input'
 import { createConversation } from "@/lib/createConversation";
+import { sendQuery } from "@/lib/query";
 import { useState, useEffect } from 'react'
 
 const apiRoute = 'http://10.147.20.73:8000'
@@ -11,11 +12,12 @@ const apiRoute = 'http://10.147.20.73:8000'
 export default function Home() {
   const [query, setQuery] = useState('')
   const [conversationId, setConversationId] = useState<string>('')
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const getConversationId = async () => {
       try {
-        const id = createConversation(apiRoute)
+        const id = await createConversation(apiRoute)
         setConversationId(id)
       } catch (error) {
         console.error("Failed setting conversation id: ", error)
@@ -26,6 +28,18 @@ export default function Home() {
     getConversationId()
   }, [])
 
+  const sendQueryHandler = async () => {
+    if (conversationId) {
+      try {
+        await sendQuery(apiRoute, query, conversationId)
+      } catch (error) {
+        console.error('Error in sendQueryHandler: ', error)
+      }
+    } else {
+      console.error("No conversation id to send.")
+    }
+  }
+
   return (
     <main className="flex min-h-screen w-full flex-col items-center bg-fixed text-accent font-sans relative"
     >
@@ -34,7 +48,7 @@ export default function Home() {
         <h1 className="text-6xl"><span className="text-primary">YUME</span> TRAVEL</h1>
         <h2 className="text-xl">"Your Personal AI Travel Advisor"</h2>
         <Input type="text" placeholder="Where do you want to go?" className="w-1/3 bg-grey border-none text-lg p-8 rounded-lg" onChange={(e) => setQuery(e.target.value)} />
-        <Button className="text-lg py-8 px-10 bg-grey hover:bg-primary">
+        <Button className="text-lg py-8 px-10 bg-grey hover:bg-primary" onClick={sendQueryHandler}>
           <span>Ask YuMe</span>
 
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
--- lib/createConversation.ts
+++ lib/createConversation.ts
@@ -1,9 +1,8 @@
-export async function createConversation(path: string): Promise<string> {
+export async function createConversation(path: string) {
   try {
     const res = await fetch(path + "/create_conversation")
     const data = await res.json()
 
-    console.log(data.conversation_id)
     return data.conversation_id
   } catch (error) {
     console.error('Error creating a conversation: ', error)
